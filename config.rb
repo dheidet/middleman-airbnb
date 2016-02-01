d16308c4235c121c8da179c425d07e93c31c66d6
dheidet <dimitri@36quaidesaromes.fr>
2016-02-01 17:13:59 +0100

    middleman dynamic page

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+["anne", "seb", "romain","anne1", "seb1", "romain1"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
 ###
 # Compass
 ###
